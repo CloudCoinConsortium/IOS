e01d1c7d56f32350679aa9007915097957ca4259
Moumita China <moumita@ascentspark.com>
2021-11-01 15:21:19 +0530

    Initial Commit

---
+++ cloudcoin/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  cloudcoin
+//
+//  Created by Moumita China on 01/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
